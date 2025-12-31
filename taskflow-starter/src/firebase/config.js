4d33b47deab07dc8984c5fc2714eb5f305b8e51f
ikraammel <ikramelhoul@gmail.com>
2025-12-31 17:20:33 +0100

    config firebase

--- taskflow-starter/src/firebase/config.js
+++ taskflow-starter/src/firebase/config.js
@@ -1,19 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Étudiants, remplacez ceci par votre propre config Firebase
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "REMPLACER_PAR_VOTRE_API_KEY",
-  authDomain: "votre-projet.firebaseapp.com",
-  projectId: "votre-projet",
-  storageBucket: "votre-projet.appspot.com",
-  messagingSenderId: "00000000000",
-  appId: "1:0000000000:web:000000000000"
+  apiKey: "AIzaSyBzBenh-fOk1t2uvHYOpIkP9JN2L7BgoLA",
+  authDomain: "project-manager-67a0e.firebaseapp.com",
+  projectId: "project-manager-67a0e",
+  storageBucket: "project-manager-67a0e.firebasestorage.app",
+  messagingSenderId: "195791382898",
+  appId: "1:195791382898:web:f675f575b77c3fbc70d5eb",
+  measurementId: "G-ZCG37M9TY8"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { auth, db };
+const analytics = getAnalytics(app);
